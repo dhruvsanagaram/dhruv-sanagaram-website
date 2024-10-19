99e884eed47e4594ea5f06cab6286fcf894fb6b4
dhruvsanagaram <dhruvsanagaram@gmail.com>
2024-10-18 21:20:21 -0500

    Fixed Vercel issues in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 // app/home.tsx
 "use client";
 
-import { useState, useEffect } from 'react';
-import * as Toggle from '@radix-ui/react-toggle';
-import { FaGithub, FaDiscord } from 'react-icons/fa';
 import HomeLanding from './home';
 
 export default function Home() {
